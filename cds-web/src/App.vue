e0ce7c27b822e3d57d7b92dc5799feba370fb3c1
NaiXiaNO1 <32020391+NaiXiaNO1@users.noreply.github.com>
2019-04-11 15:19:38 +0800

    cds
    cds

---
+++ cds-web/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="header">
+        h
+    </div>
+    <div class="tab">
+        t
+    </div>
+    <div class="content">
+        c
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import header from 'components/header/header.vue';
+  export default {
+    components:{
+      header
+    }
+
+  };
+
+</script>
+
+<style>
+
+</style>
